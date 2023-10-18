d1da4cc204930b3e3f1c2067afb9ec34b3590791
Raju kumar <117720041+Senrajkumar1998@users.noreply.github.com>
2023-10-18 16:46:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function threeSum(arr, target) {
 // write your code here
+	let n=arr.length;
+	let initialDiff=Infinity;
+	let ans=-1;
+	for (let i = 0; i < n-3; i++) {
+		for (let j = i+1; j < n-2; j++){
+			for (let k = j+1; k < n-1; k++) {
+				let sum= arr[i]+arr[j]+arr[k];
+				let diff=Math.abs(sum-target);
+				if(initialDiff>diff){
+					ans=sum;
+				}
+			}
+		}
+		
+	}
+	console.log(ans);
   
 }
 
-module.exports = threeSum;
+
